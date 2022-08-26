ca86edc353088e53cb4666b9a07e67fa90360dca
Bl-abdelatif <rahimlatif2021@gmail.com>
2022-08-26 23:08:16 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -2,11 +2,11 @@
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
 //######################################################################
-let exe1: number;
-exe1 = parseInt(prompt('write the number her'));
-if (exe1 / 2) {
-  console.log('زوجي');
-} else exe1! / 2;
-{
-  console.log('فردي');
+let exe1, exe2, result: number;
+exe1 = parseInt(prompt('write the first number '));
+exe2 = parseInt(prompt('write the second number'));
+if (exe1 < exe2) {
+  console.log((result = exe2 - exe1));
+} else {
+  console.log((result = exe1 - exe2));
 }
